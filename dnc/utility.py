f75a8b46d8adaca845df3fcff250cedaac9f7ced
Mostafa Samir <mostafa.3210@gmail.com>
2016-10-25 00:38:01 +0200

    made pairwise_add work with batches

--- dnc/utility.py
+++ dnc/utility.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def pairwise_add(u, v=None):
+def pairwise_add(u, v=None, is_batch=False):
     """
     performs a pairwise summation between vectors (possibly the same)
 
@@ -8,16 +8,19 @@ def pairwise_add(u, v=None):
     ----------
     u: Tensor (n, ) | (n, 1)
     v: Tensor (n, ) | (n, 1) [optional]
+    is_batch: bool
+        a flag for whether the vectors come in a batch
+        ie.: whether the vectors has a shape of (b,n) or (b,n,1)
 
     Returns: Tensor (n, n)
     Raises: ValueError
     """
     u_shape = u.get_shape().as_list()
 
-    if len(u_shape) > 2:
+    if len(u_shape) > 2 and not is_batch:
         raise ValueError("Expected at most 2D tensors, but got %dD" % len(u_shape))
-    if len(u_shape) == 2 and u_shape[1] != 1:
-        raise ValueError("Expected a vector, but got a matrix")
+    if len(u_shape) > 3 and is_batch:
+        raise ValueError("Expected at most 2D tensor batches, but got %dD" % len(u_shape))
 
     if v is None:
         v = u
@@ -26,15 +29,15 @@ def pairwise_add(u, v=None):
         if u_shape != v_shape:
             raise VauleError("Shapes %s and %s do not match" % (u_shape, v_shape))
 
-    n = u_shape[0]
+    n = u_shape[0] if not is_batch else u_shape[1]
 
-    column_u = tf.reshape(u, (-1, 1))
-    U = tf.concat(1, [column_u] * n)
+    column_u = tf.reshape(u, (-1, 1) if not is_batch else (-1, n, 1))
+    U = tf.concat(1 if not is_batch else 2, [column_u] * n)
 
     if v is u:
-        return U + tf.transpose(U)
+        return U + tf.transpose(U, None if not is_batch else [0, 2, 1])
     else:
-        row_v = tf.reshape(v, (1, -1))
-        V = tf.concat(0, [row_v] * n)
+        row_v = tf.reshape(v, (1, -1) if not is_batch else (-1, 1, n))
+        V = tf.concat(0 if not is_batch else 1, [row_v] * n)
 
         return U + V
--- unit-tests/utility.py
+++ unit-tests/utility.py
@@ -29,5 +29,27 @@ class DNCUtilityTests(unittest.TestCase):
                 self.assertTrue(np.allclose(UV, predicted_UV))
 
 
+    def test_pairwise_add_with_batch(self):
+        graph = tf.Graph()
+        with graph.as_default():
+            with tf.Session(graph=graph) as session:
+
+                _u = np.array([[5, 6], [7, 8]])
+                _v = np.array([[1, 2], [3, 4]])
+
+                predicted_U = np.array([[[10, 11], [11, 12]], [[14, 15], [15, 16]]])
+                predicted_UV = np.array([[[6, 7], [7, 8]], [[10, 11], [11, 12]]])
+
+                u = tf.constant(_u)
+                v = tf.constant(_v)
+
+                U_op = util.pairwise_add(u, is_batch=True)
+                UV_op = util.pairwise_add(u, v, is_batch=True)
+
+                U, UV = session.run([U_op, UV_op])
+
+                self.assertTrue(np.allclose(U, predicted_U))
+                self.assertTrue(np.allclose(UV, predicted_UV))
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
