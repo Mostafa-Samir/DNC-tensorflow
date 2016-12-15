ab7a51c125a1cc4a9dc20688f8f988054e419d1f
Mostafa Samir <mostafa.3210@gmail.com>
2016-12-15 18:37:09 +0200

    added bAbI task files

---
+++ tasks/babi/recurrent_controller.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tensorflow as tf
+from dnc.controller import BaseController
+
+"""
+A 1-layer LSTM recurrent neural network with 256 hidden units
+Note: the state of the LSTM is not saved in a variable becuase we want
+the state to reset to zero on every input sequnece
+"""
+
+class RecurrentController(BaseController):
+
+    def network_vars(self):
+        self.lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(256)
+        self.state = self.lstm_cell.zero_state(self.batch_size, tf.float32)
+
+    def network_op(self, X, state):
+        X = tf.convert_to_tensor(X)
+        return self.lstm_cell(X, state)
+
+    def get_state(self):
+        return self.state
+
+    def update_state(self, new_state):
+        return tf.no_op()
---
+++ tasks/babi/train.py
@@ -0,0 +1,164 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import tensorflow as tf
+import numpy as np
+import pickle
+import getopt
+import sys
+import os
+
+from dnc.dnc import DNC
+from recurrent_controller import RecurrentController
+
+def llprint(message):
+    sys.stdout.write(message)
+    sys.stdout.flush()
+
+def load(path):
+    return pickle.load(open(path, 'rb'))
+
+def onehot(index, size):
+    vec = np.zeros(size, dtype=np.float32)
+    vec[index] = 1.0
+    return vec
+
+def prepare_sample(sample, target_code, word_space_size, batch_size):
+    input_vec = np.array(sample[0]['inputs'], dtype=np.float32)
+    output_vec = np.array(sample[0]['inputs'], dtype=np.float32)
+    seq_len = input_vec.shape[0]
+    weights_vec = np.zeros(seq_len, dtype=np.float32)
+
+    target_mask = (input_vec == target_code)
+    output_vec[target_mask] = sample[0]['outputs']
+    weights_vec[target_mask] = 1.0
+
+    input_vec = np.apply_along_axis(onehot, 0, input_vec)
+    output_vec = np.apply_along_axis(onehot, 0, output_vec)
+
+    return (
+        np.reshape(input_vec, (1, -1, -1)),
+        np.reshape(output_vec, (1, -1, -1)),
+        seq_len,
+        np.reshape(weights_vec, (1, -1, 1))
+    )
+
+
+
+if __name__ == '__main__':
+
+    dirname = os.path.dirname(__file__)
+    ckpts_dir = os.path.join(dirname , 'checkpoints')
+    data_dir = os.path.join(dirname, 'data', 'en-10k')
+    tb_logs_dir = os.path.join(dirname, 'logs')
+
+    llprint("Loading Data ... ")
+    lexicon_dict = load(os.path.join(data_dir, 'lexicon-dict.pkl'))
+    data = load(os.path.join(data_dir, 'train', 'train.pkl'))
+    llprint("Done!\n")
+
+    batch_size = 1
+    input_size = output_size = len(lexicon_dict)
+    sequence_max_length = 100
+    word_space_size = len(lexicon_dict)
+    words_count = 256
+    word_size = 32
+    read_heads = 4
+
+    learning_rate = 1e-4
+    momentum = 0.9
+
+    from_checkpoint = None
+    iterations = 100000
+
+    options,_ = getopt.getopt(sys.argv[1:], '', ['checkpoint=', 'iterations='])
+
+    for opt in options:
+        if opt[0] == '--checkpoint':
+            from_checkpoint = opt[1]
+        elif opt[0] == '--iterations':
+            iterations = int(opt[1])
+
+    graph = tf.Graph()
+
+    with graph.as_default():
+        with tf.Session(graph=graph) as session:
+
+            llprint("Building Computational Graph ... ")
+
+            optimizer = tf.train.RMSPropOptimizer(learning_rate, momentum=momentum)
+            summerizer = tf.train.SummaryWriter(tb_logs_dir, session.graph)
+
+            ncomputer = DNC(
+                RecurrentController,
+                input_size,
+                output_size,
+                sequence_max_length,
+                words_count,
+                word_size,
+                read_heads,
+                batch_size
+            )
+
+            output, _ = ncomputer.get_outputs()
+
+            loss_weights = tf.placeholder(tf.float32, [batch_size, None, 1])
+            loss = loss_weights * tf.nn.softmax_cross_entropy_with_logits(output, ncomputer.target_output)
+
+            gradients = optimizer.compute_gradients(loss)
+            for i, (grad, var) in enumerate(gradients):
+                if grad is not None:
+                    gradients[i] = (tf.clip_by_value(grad, -10, 10), var)
+
+            apply_gradients = optimizer.apply_gradients(gradients)
+
+            summerize_loss = tf.scalar_summary("Loss", loss)
+
+            summerize_op = tf.merge_summary([summerize_loss])
+            no_summerize = tf.no_op()
+
+            llprint("Done!\n")
+
+            llprint("Initializing Variables ... ")
+            session.run(tf.initialize_all_variables())
+            llprint("Done!\n")
+
+            if from_checkpoint is not None:
+                llprint("Restoring Checkpoint %s ... " % (from_checkpoint))
+                ncomputer.restore(session, ckpts_dir, from_checkpoint)
+                llprint("Done!\n")
+
+
+            last_100_losses = []
+
+            for i in xrange(iterations + 1):
+                llprint("\rIteration %d/%d" % (i, iterations))
+
+                sample = np.random.choice(data, 1)
+                input_data, target_output, seq_len, weights = prepare_sample(sample, lexicon_dict['-'], word_space_size)
+
+                summerize = (i % 100 == 0)
+                take_checkpoint = (i != 0) and (i % iterations == 0)
+
+                loss_value, _, summary = session.run([
+                    loss,
+                    apply_gradients,
+                    summerize_op if summerize else no_summerize
+                ], feed_dict={
+                    ncomputer.input_data: input_data,
+                    ncomputer.target_output: target_output,
+                    ncomputer.sequence_length: seq_len,
+                    loss_weights: weights
+                })
+
+                last_100_losses.append(loss_value)
+                summerizer.add_summary(summary, i)
+
+                if summerize:
+                    llprint("\n\tAvg. Logistic Loss: %.4f\n" % (np.mean(last_100_losses)))
+                    last_100_losses = []
+
+                if take_checkpoint:
+                    llprint("\nSaving Checkpoint ... "),
+                    ncomputer.save(session, ckpts_dir, 'step-%d' % (i))
+                    llprint("Done!\n")
