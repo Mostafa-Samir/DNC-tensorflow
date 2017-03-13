4ed4262cda01018133080d58ef744a3425f50590
Yu-chun Huang <code@yullage.com>
2017-03-13 14:21:03 +0800

    Update utility.py

--- dnc/utility.py
+++ dnc/utility.py
@@ -34,53 +34,6 @@ def pairwise_add(u, v=None, is_batch=False):
     n = u_shape[0] if not is_batch else u_shape[1]
 
     column_u = tf.reshape(u, (-1, 1) if not is_batch else (-1, n, 1))
-    U = tf.concat(1 if not is_batch else 2, [column_u] * n)
-
-    if v is u:
-        return U + tf.transpose(U, None if not is_batch else [0, 2, 1])
-    else:
-        row_v = tf.reshape(v, (1, -1) if not is_batch else (-1, 1, n))
-        V = tf.concat(0 if not is_batch else 1, [row_v] * n)
-
-        return U + V
-
-
-import tensorflow as tf
-import numpy as np
-from tensorflow.python.ops import gen_state_ops
-
-def pairwise_add(u, v=None, is_batch=False):
-    """
-    performs a pairwise summation between vectors (possibly the same)
-
-    Parameters:
-    ----------
-    u: Tensor (n, ) | (n, 1)
-    v: Tensor (n, ) | (n, 1) [optional]
-    is_batch: bool
-        a flag for whether the vectors come in a batch
-        ie.: whether the vectors has a shape of (b,n) or (b,n,1)
-
-    Returns: Tensor (n, n)
-    Raises: ValueError
-    """
-    u_shape = u.get_shape().as_list()
-
-    if len(u_shape) > 2 and not is_batch:
-        raise ValueError("Expected at most 2D tensors, but got %dD" % len(u_shape))
-    if len(u_shape) > 3 and is_batch:
-        raise ValueError("Expected at most 2D tensor batches, but got %dD" % len(u_shape))
-
-    if v is None:
-        v = u
-    else:
-        v_shape = v.get_shape().as_list()
-        if u_shape != v_shape:
-            raise VauleError("Shapes %s and %s do not match" % (u_shape, v_shape))
-
-    n = u_shape[0] if not is_batch else u_shape[1]
-
-    column_u = tf.reshape(u, (-1, 1) if not is_batch else (-1, n, 1))
     U = tf.concat([column_u] * n, 1 if not is_batch else 2)
 
     if v is u:
